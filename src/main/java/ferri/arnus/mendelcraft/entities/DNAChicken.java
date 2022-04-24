9d478206f10e26db6ecef7d616f2ad814aa2897b
ferriarnus <61201275+ferriarnus@users.noreply.github.com>
2022-04-24 22:59:39 +0200

    recipe

--- src/main/java/ferri/arnus/mendelcraft/entities/DNAChicken.java
+++ src/main/java/ferri/arnus/mendelcraft/entities/DNAChicken.java
@@ -1,20 +1,34 @@
 package ferri.arnus.mendelcraft.entities;
 
+import javax.annotation.Nullable;
+
 import ferri.arnus.mendelcraft.capability.DNAProvider;
 import ferri.arnus.mendelcraft.capability.DNAStorage;
 import ferri.arnus.mendelcraft.capability.IDNAStorage;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.AgeableMob;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Saddleable;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.animal.Chicken;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.util.LazyOptional;
 
-public class DNAChicken extends Chicken{
+public class DNAChicken extends Chicken implements Saddleable{
 	private IDNAStorage storage = DNAStorage.random(this.level);
+	private boolean saddled;
 	
 	public DNAChicken(EntityType<? extends Chicken> p_28236_, Level p_28237_) {
 		super(EntityRegistry.DNACHICKEN.get(), p_28237_);
@@ -49,18 +63,6 @@ public class DNAChicken extends Chicken{
 		this.getCapability(DNAProvider.DNASTORAGE).ifPresent(s -> s.deserializeNBT(p_28243_.getCompound("DNA")));
 	}
 	
-//	@Override
-//	public boolean save(CompoundTag pCompound) {
-//		this.getCapability(DNAProvider.DNASTORAGE).ifPresent(s -> pCompound.put("DNA", s.serializeNBT()));
-//		return super.save(pCompound);
-//	}
-	
-//	@Override
-//	public void load(CompoundTag nbt) {
-//		this.getCapability(DNAProvider.DNASTORAGE).ifPresent(s -> s.deserializeNBT(nbt.getCompound("DNA")));
-//		super.load(nbt);
-//	}
-	
 	@Override
 	public <T> LazyOptional<T> getCapability(Capability<T> cap) {
 		if (cap == DNAProvider.DNASTORAGE) {
@@ -68,5 +70,84 @@ public class DNAChicken extends Chicken{
 		}
 		return super.getCapability(cap);
 	}
-
+	
+	@Override
+	public boolean isSaddleable() {
+		return true;
+	}
+	
+	@Override
+	public void equipSaddle(SoundSource p_21748_) {
+		this.saddled = true;
+		
+	}
+	
+	@Override
+	public boolean isSaddled() {
+		return this.saddled;
+	}
+	
+	protected void doPlayerRide(Player p_30634_) {
+		if (!this.level.isClientSide) {
+			p_30634_.setYRot(this.getYRot());
+			p_30634_.setXRot(this.getXRot());
+			p_30634_.startRiding(this);
+		}
+		
+	}
+	
+	@Override
+	public void travel(Vec3 p_30633_) {
+		if (this.isAlive()) {
+			if (this.isVehicle() && this.canBeControlledByRider() && this.isSaddled()) {
+				LivingEntity livingentity = (LivingEntity)this.getControllingPassenger();
+				this.setYRot(livingentity.getYRot());
+				this.yRotO = this.getYRot();
+				this.setXRot(livingentity.getXRot() * 0.5F);
+				this.setRot(this.getYRot(), this.getXRot());
+				this.yBodyRot = this.getYRot();
+				this.yHeadRot = this.yBodyRot;
+				float f = livingentity.xxa * 0.5F;
+	            float f1 = livingentity.zza;
+				
+				if (this.isControlledByLocalInstance()) {
+					this.setSpeed((float)this.getAttributeValue(Attributes.MOVEMENT_SPEED));
+					super.travel(new Vec3((double)f, p_30633_.y, (double)f1));
+				} else if (livingentity instanceof Player) {
+					this.setDeltaMovement(Vec3.ZERO);
+				}
+				
+				this.calculateEntityAnimation(this, false);
+				this.tryCheckInsideBlocks();
+			} else {
+				System.out.println("why");
+				this.flyingSpeed = 0.02F;
+				super.travel(p_30633_);
+			}
+		}
+	}
+	
+	@Override
+	public InteractionResult mobInteract(Player p_27584_, InteractionHand p_27585_) {
+		ItemStack itemstack = p_27584_.getItemInHand(p_27585_);
+		if (this.isVehicle()) {
+			return super.mobInteract(p_27584_, p_27585_);
+		}
+		if (itemstack.is(Items.SADDLE)) {
+			this.saddled = true;
+			return InteractionResult.sidedSuccess(this.level.isClientSide);
+		}
+		this.doPlayerRide(p_27584_);
+		return InteractionResult.sidedSuccess(this.level.isClientSide);
+	}
+	
+	public boolean canBeControlledByRider() {
+		return this.getControllingPassenger() instanceof LivingEntity;
+	}
+	
+	@Nullable
+	public Entity getControllingPassenger() {
+		return this.getFirstPassenger();
+	}
+	
 }
