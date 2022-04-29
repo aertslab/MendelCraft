ceca8ae2de32386f5737488d881b2bc5c04674ab
ferriarnus <61201275+ferriarnus@users.noreply.github.com>
2022-04-29 13:22:00 +0200

    rideable chicken + jump

--- src/main/java/ferri/arnus/mendelcraft/entities/DNAChicken.java
+++ src/main/java/ferri/arnus/mendelcraft/entities/DNAChicken.java
@@ -4,31 +4,34 @@ import javax.annotation.Nullable;
 
 import ferri.arnus.mendelcraft.capability.DNAProvider;
 import ferri.arnus.mendelcraft.capability.DNAStorage;
+import ferri.arnus.mendelcraft.capability.DNAUtil;
 import ferri.arnus.mendelcraft.capability.IDNAStorage;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.Saddleable;
+import net.minecraft.world.entity.PlayerRideableJumping;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.animal.Chicken;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.util.LazyOptional;
 
-public class DNAChicken extends Chicken implements Saddleable{
+public class DNAChicken extends Chicken implements PlayerRideableJumping{
 	private IDNAStorage storage = DNAStorage.random(this.level);
-	private boolean saddled;
+	protected boolean isJumping;
+	protected float playerJumpPendingScale;
+
+
 	
 	public DNAChicken(EntityType<? extends Chicken> p_28236_, Level p_28237_) {
 		super(EntityRegistry.DNACHICKEN.get(), p_28237_);
@@ -71,22 +74,6 @@ public class DNAChicken extends Chicken implements Saddleable{
 		return super.getCapability(cap);
 	}
 	
-	@Override
-	public boolean isSaddleable() {
-		return true;
-	}
-	
-	@Override
-	public void equipSaddle(SoundSource p_21748_) {
-		this.saddled = true;
-		
-	}
-	
-	@Override
-	public boolean isSaddled() {
-		return this.saddled;
-	}
-	
 	protected void doPlayerRide(Player p_30634_) {
 		if (!this.level.isClientSide) {
 			p_30634_.setYRot(this.getYRot());
@@ -99,7 +86,7 @@ public class DNAChicken extends Chicken implements Saddleable{
 	@Override
 	public void travel(Vec3 p_30633_) {
 		if (this.isAlive()) {
-			if (this.isVehicle() && this.canBeControlledByRider() && this.isSaddled()) {
+			if (this.isVehicle() && this.canBeControlledByRider() && DNAUtil.isBig(this)) {
 				LivingEntity livingentity = (LivingEntity)this.getControllingPassenger();
 				this.setYRot(livingentity.getYRot());
 				this.yRotO = this.getYRot();
@@ -110,6 +97,22 @@ public class DNAChicken extends Chicken implements Saddleable{
 				float f = livingentity.xxa * 0.5F;
 	            float f1 = livingentity.zza;
 				
+	            if (this.playerJumpPendingScale > 0.0F && this.onGround && !this.isJumping()) {
+	            	double d0 = (double)this.playerJumpPendingScale * (double)this.getBlockJumpFactor();
+	            	double d1 = d0 + this.getJumpBoostPower();
+	            	Vec3 vec3 = this.getDeltaMovement();
+	            	this.setDeltaMovement(vec3.x, d1, vec3.z);
+	            	this.setIsJumping(true);
+	            	this.hasImpulse = true;
+	            	net.minecraftforge.common.ForgeHooks.onLivingJump(this);
+	            	if (f1 > 0.0F) {
+	            		float f2 = Mth.sin(this.getYRot() * ((float)Math.PI / 180F));
+	            		float f3 = Mth.cos(this.getYRot() * ((float)Math.PI / 180F));
+	            		this.setDeltaMovement(this.getDeltaMovement().add((double)(-0.4F * f2 * this.playerJumpPendingScale), 0.0D, (double)(0.4F * f3 * this.playerJumpPendingScale)));
+	            	}
+	            	this.playerJumpPendingScale = 0.0F;
+	            }
+	            
 				if (this.isControlledByLocalInstance()) {
 					this.setSpeed((float)this.getAttributeValue(Attributes.MOVEMENT_SPEED));
 					super.travel(new Vec3((double)f, p_30633_.y, (double)f1));
@@ -117,28 +120,36 @@ public class DNAChicken extends Chicken implements Saddleable{
 					this.setDeltaMovement(Vec3.ZERO);
 				}
 				
+				if (this.onGround) {
+					this.playerJumpPendingScale = 0.0F;
+					this.setIsJumping(false);
+				}
+				
 				this.calculateEntityAnimation(this, false);
 				this.tryCheckInsideBlocks();
 			} else {
-				System.out.println("why");
 				this.flyingSpeed = 0.02F;
 				super.travel(p_30633_);
 			}
 		}
 	}
 	
+	private boolean isJumping() {
+		// TODO Auto-generated method stub
+		return this.isJumping;
+	}
+
+	private void setIsJumping(boolean b) {
+		this.isJumping = b;
+	}
+
 	@Override
 	public InteractionResult mobInteract(Player p_27584_, InteractionHand p_27585_) {
 		ItemStack itemstack = p_27584_.getItemInHand(p_27585_);
-		if (this.isVehicle()) {
-			return super.mobInteract(p_27584_, p_27585_);
+		if (itemstack.isEmpty() && DNAUtil.isBig(this)) {
+			this.doPlayerRide(p_27584_);
 		}
-		if (itemstack.is(Items.SADDLE)) {
-			this.saddled = true;
-			return InteractionResult.sidedSuccess(this.level.isClientSide);
-		}
-		this.doPlayerRide(p_27584_);
-		return InteractionResult.sidedSuccess(this.level.isClientSide);
+		return super.mobInteract(p_27584_, p_27585_);
 	}
 	
 	public boolean canBeControlledByRider() {
@@ -148,6 +159,33 @@ public class DNAChicken extends Chicken implements Saddleable{
 	@Nullable
 	public Entity getControllingPassenger() {
 		return this.getFirstPassenger();
+	}
+
+	@Override
+	public void onPlayerJump(int p_21696_) {
+		if (p_21696_ < 0) {
+			p_21696_ = 0;
+		}
+		if (p_21696_ >= 90) {
+			this.playerJumpPendingScale = 1.0F;
+		} else {
+			this.playerJumpPendingScale = 0.4F + 0.4F * (float)p_21696_ / 90.0F;
+		}
+	}
+
+	@Override
+	public boolean canJump() {
+		return DNAUtil.isBig(this);
+	}
+
+	@Override
+	public void handleStartJump(int p_21695_) {
+		
+	}
+
+	@Override
+	public void handleStopJump() {
+		
 	}
 	
 }
